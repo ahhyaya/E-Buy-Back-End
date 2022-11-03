2604aba772dc2f09071aa53df7ed2877c18846ad
doldol123 <toruig59@gmail.com>
2022-11-02 20:24:39 -0700

    complete find product by id route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,9 +19,23 @@ router.get('/', async (req, res) => {
 
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag}],
+    });
+
+    if (!product) {
+      res.status(404).json({message: 'No product found with this ID!'});
+      return;
+    }
+
+    res.status(200).json(product);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
