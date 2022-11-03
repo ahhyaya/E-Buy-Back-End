45f3080733140c3427839a9bb03cf0f866899ee2
doldol123 <toruig59@gmail.com>
2022-11-02 20:17:45 -0700

    complete delete category by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,10 +49,27 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+    try {
+      const category = await Category.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      if (!category) {
+        res.status(404).json({message: 'No category found with this ID!'});
+        return;
+      }
+
+      res.status(200).json(category);
+    } catch(err) {
+      res.status(500).json(err);
+    }
 });
 
 module.exports = router;
