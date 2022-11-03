32ac824dfc8aae8af82fb70372fc7e4633442be6
doldol123 <toruig59@gmail.com>
2022-11-02 20:32:12 -0700

    complete delete tag by id route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -49,8 +49,24 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tag) {
+      res.status(404).json({message: 'No tag found with this ID!'});
+      return;
+    }
+
+    res.status(200).json(tag);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
