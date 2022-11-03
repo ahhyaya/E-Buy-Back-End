4f4e1318106500ed768210f0c4352119234e846d
doldol123 <toruig59@gmail.com>
2022-11-02 20:12:30 -0700

    complete find one category by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,18 +9,32 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categories = await Category.findAll({ 
+    const category = await Category.findAll({ 
       include: [{model: Product}],
     });
-    res.status(200),json(categories);
+    res.status(200),json(category);
   }catch (err) {
     res.status(500).json(err);
   }
   });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+
+    if (!category) {
+      res.status(404).json({message: 'No category found with this ID!'});
+      return;
+    }
+
+    res.status(200).json(category);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
